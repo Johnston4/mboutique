cf07bb463e54eca7f55eef8500451fd0e4b3b17d
Sara Johnston <sarajohnston4@gmail.com>
2015-11-25 11:59:46 -0800

    printted list

--- main.php
+++ main.php
@@ -1,29 +1,17 @@
 <?php
-$menu = array("about"=>
+$menu = array("about" =>
     array('link_text' => 'Home', 'link_url' => 'index.html'),
-    "our_macarons"=>
-        array('link_text' => 'Our Macarons','link_url' => 'our_macarons.html'),
-    "gifts_parties"=>
-        array('link_text' => 'Gifts & Parties','link_url' => 'gifts_parties.html'),
-    "contact"=>
+    "our_macarons" =>
+        array('link_text' => 'Our Macarons', 'link_url' => 'our_macarons.html'),
+    "gifts_parties" =>
+        array('link_text' => 'Gifts & Parties', 'link_url' => 'gifts_parties.html'),
+    "contact" =>
         array('link_text' => 'Contact Us', 'link_url' => 'contact.html'),
 );
-
-foreach($menu as $value){
-    print(
-    "<li>
-<a href='{$value['link_url']}'>{$value['link_text']}<
-/a>
-	   </li>");
+foreach ($menu as $value) {
     ?>
     <li>
-        <a href="<?php print($value['link_url']);?>"><?php print($value['link_text']);?></a>
+        <a href="<?= $value['link_url']; ?>"><?= $value['link_text']; ?></a>
     </li>
     <?php
-    ?>
-    <li>
-        <a href="<?=$value['link_url'];?>"><?=$value['link_text'];?></a>
-    </li>
-    <?php
-
 }

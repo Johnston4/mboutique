8dac6d5b37bcaa78ae9c0c180c66fac0a979d3b4
Sara Johnston <sarajohnston4@gmail.com>
2015-12-01 16:27:36 -0800

    contact form done

---
+++ contact.php
@@ -0,0 +1,68 @@
+<img src="assets/images/contact-image.png" id="contact">
+<div id="page4">
+    <div id="page4_info">
+        <p class="p_title" id="p_mobile">
+            Visit us!
+        </p>
+        <p>
+            Monday - Friday | 10am - 9pm<br>
+            Saturday | 10am - 8pm<br>
+            Sunday | 11am - 7pm<br>
+            Closed Thanksgiving Day, Christmas Day and<br> Easter Day
+        </p>
+        <p>
+            1625 Post St<br>San Francisco, CA 94115
+        </p>
+        <p>
+            949-800-3111
+        </p>
+        <p>
+            <a class="email" href="mailto:order@mboutique.com">order@mboutique.com</a>
+        </p>
+        <p>
+            Send your questions, comments and flavor suggestions or place an order!
+        </p>
+    </div>
+    <div id="page4_contact">
+        <p class="p_title">
+            Contact Form
+        </p>
+        <?php
+        if(!empty($_POST)){
+            $name= $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+        }
+        else{
+            $name='';
+            $email ='';
+            $phone ='';
+            $subject = '';
+            $message = '';
+        }
+            ?>
+            <form id="page4_form" action="index.php?page=contact" method="post">
+            <input type="text" name="name" placeholder="Name" value="<?=$name;?>">
+            <input type="text" name="email" placeholder="Email" value="<?=$email;?>">
+            <input type="text" name="phone" placeholder="Phone" value="<?=$phone;?>">
+            <input type="text" name="subject" placeholder="Subject" value="<?=$subject;?>">
+            <textarea name="message" placeholder="Message"><?=$message;?></textarea>
+            <button name="submit">Submit</button>
+        </form>
+        <?php
+
+
+        ?>
+
+
+
+
+
+
+    </div>
+    <div>
+        <img class="image_hide" id="image" src="assets/images/macarons-image.png">
+    </div>
+</div>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $menu = array(
     "about" => array('link_text' => 'Home', 'link_url' => 'index.html', 'default' => true),
     "our_macarons" => array('link_text' => 'Our Macarons', 'link_url' => 'our_macarons.html'),
     "gifts_parties" => array('link_text' => 'Gifts & Parties', 'link_url' => 'gifts_parties.html'),
-    "contact" => array('link_text' => 'Contact Us', 'link_url' => 'contact.html'),
+    "contact" => array('link_text' => 'Contact Us', 'link_url' => 'contact.php'),
 );
 
 ?>
@@ -22,7 +22,7 @@ if(empty($_GET['page'])){
     include_once($menu[$page]['link_url']);
 }
 else{
-    if($_GET['page']=='our_macarons'||$_GET['page']=='gifts_parties'||$_GET['page']=='about'||$_GET['page']=='contact'){
+    if(isset($menu[$_GET['page']])){
         $page = $_GET['page'];
         include_once($menu[$page]['link_url']);
     }
@@ -30,7 +30,6 @@ else{
         include_once('404.php');
     }
 }
-
 ?>
 <?php
 include_once('footer.php');
